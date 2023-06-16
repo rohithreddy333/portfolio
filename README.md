564eccf31ef7ab0a6cad2ea14b791c1760389368
Rohith Reddy Dhoodhipala <50194466+rohithreddy333@users.noreply.github.com>
2023-06-16 09:14:17 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-Rohith's Portfolio website
--- src/components/Skills/index.tsx
+++
@@ -1,9 +0,0 @@
-import SkillsBody from "./SkillsBody";
-
-const Skills = () => {
-  return (
-    <SkillsBody />
-  )
-}
-
-export default Skills;
