a40783a8a1d66f20cdc7c63be4d51a3519d7f12b
Rohith Reddy Dhoodhipala <50194466+rohithreddy333@users.noreply.github.com>
2023-06-16 23:11:21 +0530

    files upload

---
+++ src/components/AppHeader/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppHeaderBody from "./AppHeaderBody";
+
+
+const AppHeader = () => {
+    return(
+        <AppHeaderBody />
+    )
+}
+
+export default AppHeader;
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeBody from "./HomeBody";
+
+const Home = () => {
+  return (
+    <HomeBody />
+  )
+}
+
+export default Home;
